3ef42a241e1f4e3fbe9e7977598b0bc989fe47d5
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2022-10-16 18:39:17 -0300

    Add time do platform fall[C

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Tempo que demora para a plataforma cair
+    public float fallingTime;
+
+    public BoxCollider2D boxCollider;
+    public TargetJoin joint;
+
+    void Falling()
     {
-        
+        //Desabilitar o collider e o joint target da plataforma
+        boxCollider.enabled = false;    
+        joint.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnCollisorEnter2D(Collision2D collision)
     {
-        
+        if (collision.transform.CompareTag("Player"))
+        {
+            //Chamar um método depois de um determinado tempo - invoke
+            Invoke("Falling", fallingTime);
+        }
     }
 }
